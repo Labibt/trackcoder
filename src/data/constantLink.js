67ffe2fc1ee54515e630539955770023e5b6f181
Siddharth <siddharthcpn2512@gmail.com>
2025-06-16 02:37:39 +0530

    fix: update baseUri to use production API endpoint

--- src/data/constantLink.js
+++ src/data/constantLink.js
@@ -1,5 +1,5 @@
-//export const baseUri = "https://trackcoder-api.onrender.com/api";
-export const baseUri = "http://localhost:5000/api";
+export const baseUri = "https://trackcoder-api.onrender.com/api";
+//export const baseUri = "http://localhost:5000/api";
 export const gfgUri = "https://www.geeksforgeeks.org/user/";
 export const leetcodeUri = "https://leetcode.com/u/";
-export const codechefUri = "https://www.codechef.com/users/"; 
+export const codechefUri = "https://www.codechef.com/users/";
